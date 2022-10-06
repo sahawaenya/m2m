c4c4c296d506043272173eea0b5802da891d1c3e
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-10-06 22:36:52 +0700

    Refactor match association in match detail

--- models/matchdetail.js
+++ models/matchdetail.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      MatchDetail.belongsTo(models.Event, {foreignKey: 'MatchId'});
+      MatchDetail.belongsTo(models.Match, {foreignKey: 'MatchId'});
       MatchDetail.belongsTo(models.User, {foreignKey: 'UserId'});
     }
   }
