ac6d386e7aba96f50a807634d572e4c78e55710c
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-10-09 22:56:22 +0700

    set authorization middleware

--- routes/matchRoute.js
+++ routes/matchRoute.js
@@ -1,7 +1,14 @@
 const MatchController = require("../controllers/match");
 const router = require("express").Router();
+const authHandler = require('../middleware/authHandler');
 
+router.use(authHandler);
 router.post("/matches", MatchController.addMatch);
 router.delete("/matches/:matchId", MatchController.deleteMatch);
+router.get('/matches', MatchController.getAllMatches);
+router.get('/matches/:matchId', MatchController.getMatchesById);
+router.post('/matches/:matchId/join', MatchController.joinMatches);
+router.get('/matches/:matchId/participants', MatchController.showRequestParticipants);
+router.patch('/matches/:matchId/participants/:participantId', MatchController.changeRequestParticipantsStatus);
 
 module.exports = router;
