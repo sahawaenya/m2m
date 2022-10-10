97174b7ac4d154e2b4502906e6eaec49551415ec
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-10-10 16:41:25 +0700

    Refactor authorization  handler

--- routes/matchRoute.js
+++ routes/matchRoute.js
@@ -2,13 +2,12 @@ const MatchController = require("../controllers/match");
 const router = require("express").Router();
 const authHandler = require('../middleware/authHandler');
 
-router.use(authHandler);
-router.post("/matches", MatchController.addMatch);
-router.delete("/matches/:matchId", MatchController.deleteMatch);
-router.get('/matches', MatchController.getAllMatches);
-router.get('/matches/:matchId', MatchController.getMatchesById);
-router.post('/matches/:matchId/join', MatchController.joinMatches);
-router.get('/matches/:matchId/participants', MatchController.showRequestParticipants);
-router.patch('/matches/:matchId/participants/:participantId', MatchController.changeRequestParticipantsStatus);
+router.post("/matches",authHandler, MatchController.addMatch);
+router.delete("/matches/:matchId",authHandler, MatchController.deleteMatch);
+router.get('/matches',authHandler, MatchController.getAllMatches);
+router.get('/matches/:matchId',authHandler, MatchController.getMatchesById);
+router.post('/matches/:matchId/join',authHandler, MatchController.joinMatches);
+router.get('/matches/:matchId/participants',authHandler, MatchController.showRequestParticipants);
+router.patch('/matches/:matchId/participants/:participantId',authHandler, MatchController.changeRequestParticipantsStatus);
 
 module.exports = router;
