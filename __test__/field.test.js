0a7ffccf17a5f0ce71df2f2a8d9b28e56e68aa80
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-10-11 20:32:27 +0700

    Update field.test.js

--- __test__/field.test.js
+++ __test__/field.test.js
@@ -9,15 +9,16 @@ beforeEach(() => {
 describe("/fields", () => {
   test("/fields get successful", async () => {
     const response = await request(app).get("/fields");
-    // console.log(response._body, "<<< ini fields");
+    expect(response.status).toBe(200);
+  });
+
+  test("it should return all fields list", async () => {
+    const response = await request(app).get("/fields?category=1");
     expect(response.status).toBe(200);
   });
 
   test('it should return error', async () => {
-    // User.findAll = jest.fn().mockRejectedValue('Error')
-
     jest.spyOn(Field, 'findAll').mockRejectedValue('Error')
-
     const response = await request(app).get('/fields');
     expect(response.status).toEqual(500);
   })
