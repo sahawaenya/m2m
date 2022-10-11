5d77c3a946975be578db7bac742f8872721ae8c0
Refaldy Bagas R <refaldy.bagas10@gmail.com>
2022-10-11 10:17:34 +0700

    Update field.test.js

--- __test__/field.test.js
+++ __test__/field.test.js
@@ -1,7 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
-const { sequelize } = require("../models");
-const { queryInterface } = sequelize;
+const {Field} = require("../models");
+
+beforeEach(() => {
+  jest.restoreAllMocks()
+})
 
 describe("/fields", () => {
   test("/fields get successful", async () => {
@@ -9,4 +12,13 @@ describe("/fields", () => {
     // console.log(response._body, "<<< ini fields");
     expect(response.status).toBe(200);
   });
+
+  test('it should return error', async () => {
+    // User.findAll = jest.fn().mockRejectedValue('Error')
+
+    jest.spyOn(Field, 'findAll').mockRejectedValue('Error')
+
+    const response = await request(app).get('/fields');
+    expect(response.status).toEqual(500);
+  })
 });
