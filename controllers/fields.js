0cf3e53ea60a6f04e6d9c7408a9f06db2c300231
sahawaenya <saha.waenya@gmail.com>
2022-10-26 18:02:48 +0700

    adjustment

--- controllers/fields.js
+++ controllers/fields.js
@@ -4,15 +4,19 @@ class FieldController {
   static async getFields(req, res, next) {
     try {
       const whereClause = {};
-      const {category} = req.query;
+      const { category } = req.query;
 
       if (category) {
         whereClause.CategoryId = Number(category);
       }
 
-      const fields = await Field.findAll({ include: Category , where: whereClause});
+      const fields = await Field.findAll({
+        include: Category,
+        where: whereClause,
+      });
       res.status(200).json(fields);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
